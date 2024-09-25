43a6ce6652eafb65b9927bf54797a951fd80f13a
shivamgoyal <shivam1111@gmail.com>
2024-09-25 21:32:46 +0530

    mill_production report half way

--- mill_production/models/mill_production.py
+++ mill_production/models/mill_production.py
@@ -6,14 +6,16 @@ class StockLine(models.Model):
 
     @api.onchange('pcs', 'kg_per_pc')
     def _compute_qty(self):
-        self.qty = -(self.kg_per_pc * self.pcs) / 1000
+        for line in self:
+            line.qty = -(line.kg_per_pc * line.pcs) / 1000
 
     @api.depends('scrap', 'qty')
     def _compute_scrap(self):
-        try:
-            self.scrap_percentage = (self.scrap * 100) / (1000 * self.qty)
-        except ZeroDivisionError:
-            self.scrap_percentage = 0.00
+        for line in self:
+            try:
+                line.scrap_percentage = (line.scrap * 100) / (1000 * line.qty)
+            except ZeroDivisionError:
+                line.scrap_percentage = 0.00
 
     name = fields.Char('Name')
     pcs = fields.Float('Pcs')
@@ -23,7 +25,7 @@ class StockLine(models.Model):
     kg_per_pc = fields.Float('Kg/pc')
     production_id = fields.Many2one('mill.production','Production',ondelete='cascade')
     scrap = fields.Float('Scrap')
-    scrap_percentage = fields.Float('Scrap%',compute = "_compute_scrap")
+    scrap_percentage = fields.Float('Scrap%',compute = "_compute_scrap",digits=(16, 2))
     heat_no_ids = fields.Many2many('heat.heat','stock_line_heat_heat_relation','stock_line_id','heat_id','Heats')
     production_line_id = fields.Many2one('production.order.line', 'Prooduction Line', help="This field stores the planned productio line")
 
@@ -47,52 +49,60 @@ class MillProduction(models.Model):
 
     @api.depends('total_production', 'production_line_ids.scrap')
     def _compute_scrap(self):
-        total = 0.00
-        for i in self.production_line_ids:
-            total = total + i.scrap
-        self.total_scrap = total
+        for po in self:
+            total = 0.00
+            for i in po.production_line_ids:
+                total = total + i.scrap
+            po.total_scrap = total
 
     @api.depends('total_production', 'production_line_ids.scrap')
     def _compute_scrap_percentage(self):
-        try:
-            self.scrap_percentage = (self.total_scrap * 100) / (1000 * self.total_production)
-        except ZeroDivisionError:
-            self.scrap_percentage = 0.00
+        for po in self:
+            try:
+                po.scrap_percentage = (po.total_scrap * 100) / (1000 * po.total_production)
+            except ZeroDivisionError:
+                po.scrap_percentage = 0.00
 
     @api.depends('production_line_ids', 'production_line_ids.qty')
     def _compute_total_production(self):
-        total = 0.00
-        for i in self.production_line_ids:
-            total = total + i.qty
-        self.total_production = -total
+        for po in self:
+            total = 0.00
+            for i in po.production_line_ids:
+                total = total + i.qty
+            po.total_production = -total
 
     @api.depends('kwh_closing','kwh_opening','total_production')
     def _compute_units(self):
-        self.total_units = (self.kwh_closing - self.kwh_opening)*15
+        for po in self:
+            po.total_units = (po.kwh_closing - po.kwh_opening)*15
     @api.depends('kwh_closing','kwh_opening','total_production')
     def _compute_units_mt(self):
-        try:
-            self.units_per_mt = self.total_units / self.total_production
-        except ZeroDivisionError:
-            self.units_per_mt = 0.00
+        for po in self:
+            try:
+                po.units_per_mt = po.total_units / po.total_production
+            except ZeroDivisionError:
+                po.units_per_mt = 0.00
 
     @api.depends('total_production', 'production_line_ids.scrap')
     def _compute_kwh_mt(self):
-        try:
-            self.kwh_mt = self.total_units / self.total_production
-        except ZeroDivisionError:
-            self.kwh_mt = 0.00
+        for po in self:
+            try:
+                po.kwh_mt = po.total_units / po.total_production
+            except ZeroDivisionError:
+                po.kwh_mt = 0.00
 
     @api.depends('png_units_opening', 'png_units_closing')
     def _compute_png_units(self):
-        self.png_net = self.png_units_closing - self.png_units_opening
+        for po in self:
+            po.png_net = po.png_units_closing - po.png_units_opening
 
     @api.depends('png_units_opening', 'png_units_closing')
     def _compute_png_mt(self):
-        try:
-            self.png_net_mt = self.png_net/self.total_production
-        except ZeroDivisionError:
-            self.png_net_mt = 0.00
+        for po in self:
+            try:
+                po.png_net_mt = po.png_net/po.total_production
+            except ZeroDivisionError:
+                po.png_net_mt = 0.00
 
     name = fields.Char('Name', default='/', required=True)
     date = fields.Date('Date', required=True, default=fields.Date.today)
@@ -123,5 +133,5 @@ class MillProduction(models.Model):
     kva_opening = fields.Float('KVA Op.')
     kva_closing = fields.Float('KVA Cl.')
     png_net = fields.Float("Total PNG", compute='_compute_png_units', store=True)
-    png_net_mt = fields.Float('PNG (SCM/MT)',compute="_compute_png_mt",store=True)
+    png_net_mt = fields.Float('PNG (SCM/MT)',compute="_compute_png_mt",store=True,digits=(16, 2))
     order_id = fields.Many2one('production.order', 'Production Order')
--- mill_production/report/mill_production_report.py
+++ mill_production/report/mill_production_report.py
@@ -5,14 +5,62 @@ class MillProductionReport(models.AbstractModel):
     _inherit = 'report.report_xlsx.abstract'
     _description = 'Mill Production Report'
 
-    def generate_xlsx_report(self, workbook, data, production_order):
-        if len(self) == 1:
-            print ("======There is only one object")
+    def generate_xlsx_report(self, workbook, data, po):
+        headers_row = ['Size','Net Wt','Furnace','Grade','Scrap','Scrap %']
+        summary_headers = ['','Water','Solar (KW)','Solar (KV)','KW','KV']
+
+        if len(self) <= 1:
+            def write_row(sheet,column,counter,row,format):
+                sheet.write_row('{column}{row}'.format(column=column,row=counter),row,format)
+                return counter+1
+
+            report_name = po.name
+            sheet = workbook.add_worksheet(report_name[:31])
+            header_format = workbook.add_format({'bold': True})
+            header_format.set_bottom()
+            header_format.set_align('center')
+            col_header_format = workbook.add_format({'bold': True})
+
+            row_counter = 1
+            date_format = workbook.add_format({'num_format': 'dd/mm/yy','bold':True})
+            date_format.set_align('center')
+            date_row = ['Date',po.date]
+            col_counter = len(headers_row)
+            #Adding Water Column
+            sheet.write_column(row_counter+1,col_counter,['Op.','Cl.','Net Usage'],col_header_format)
+            col_counter+=1
+            sheet.write_column(row_counter+1,col_counter,[po.water_units_opening,po.water_units_closing,(po.water_units_closing-po.water_units_opening)])
+            col_counter+=1
+            sheet.write_column(row_counter+1,col_counter,[po.water_units_opening,po.water_units_closing,(po.water_units_closing-po.water_units_opening)])
+
+            row_counter = write_row(sheet,'A',row_counter,date_row,date_format)
+            row_counter  = write_row(sheet,'A',row_counter,headers_row+summary_headers,header_format)
+            for i,line in enumerate(po.production_line_ids):
+                row_format = workbook.add_format()
+                row_format.set_align('center')
+                row = [
+                       line.size_id.name,line.qty,
+                       line.partner_id and line.partner_id.name or "-",
+                       line.grade_id and line.grade_id.name or "-",
+                       line.scrap,
+                       line.scrap_percentage,
+                   ]
+                if i == (len(po.production_line_ids) - 1):
+                    row_format.set_bottom()
+
+                row_format.set_left()
+                sheet.write_row('A%s' % row_counter, row,row_format)
+                row_counter+=1
+            # Summary Printing
+            summary_row = ['PNG/MT',po.png_net_mt,'MD/MT',po.md_mt,'Hours',po.hours]
+            summary_format = workbook.add_format({'bold':True})
+            summary_format.set_bottom()
+            summary_format.set_align('center')
+            row_counter = write_row(sheet,'A',row_counter,summary_row,summary_format)
+
         else:
-            for obj in production_order:
-                print("--------obj",obj.name,data)
+            for obj in po:
                 report_name = obj.name
-                # One sheet by partner
                 sheet = workbook.add_worksheet(report_name[:31])
                 bold = workbook.add_format({'bold': True})
                 sheet.write(0, 0, obj.name, bold)
